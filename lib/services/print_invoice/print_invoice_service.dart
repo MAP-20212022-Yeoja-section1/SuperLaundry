ca5cca4951d20560b21dcd43a7594aaab04fe335
rnabilahusna <nabilaquimby@gmail.com>
2022-07-02 09:47:01 +0800

    #37

--- lib/services/print_invoice/print_invoice_service.dart
+++ lib/services/print_invoice/print_invoice_service.dart
@@ -6,5 +6,6 @@ import '../../models/invoice.dart';
 abstract class PrintInvoiceService {
   Stream<List<OrdersModel>> readActiveOrders();
   Future<File> saveDocument(String name, Document pdf);
+  Future openFile(File file);
   Future<File> generate(Invoice invoice);
 }
